90197538b7113f4a99797f07b5c9957812a87412
SquidXTV <squidxtv@gmail.com>
2021-08-09 05:28:10 +0200

    Update PharaohRenderer.java

--- src/main/java/com/squidxtv/tlos/entity/PharaohRenderer.java
+++ src/main/java/com/squidxtv/tlos/entity/PharaohRenderer.java
@@ -9,7 +9,7 @@ public class PharaohRenderer extends BipedRenderer<PharaohEntity, PharaohModel> 
     private static final ResourceLocation TEXTURE = new ResourceLocation(TheLegendOfSteve.MODID, "textures/entity/pharaoh/pharaoh.png");
 
     public PharaohRenderer(EntityRendererManager p_i46168_1_) {
-        super(p_i46168_1_, new PharaohModel(0.5F,false), 0.5F);
+        super(p_i46168_1_, new PharaohModel(0.0F,false), 0.5F);
     }
 
 
