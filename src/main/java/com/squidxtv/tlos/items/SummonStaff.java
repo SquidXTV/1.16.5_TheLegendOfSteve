26efd9150a90ec46eda658ac2b551dd3ea146775
SquidXTV <squidxtv@gmail.com>
2021-08-09 08:50:28 +0200

    Updated

---
+++ src/main/java/com/squidxtv/tlos/items/SummonStaff.java
@@ -0,0 +1,9 @@
+package com.squidxtv.tlos.items;
+
+import net.minecraft.item.Item;
+
+public class SummonStaff extends Item {
+    public SummonStaff(Properties p_i48487_1_) {
+        super(p_i48487_1_);
+    }
+}
