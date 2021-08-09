58795fa265e06750605b61dcc9f9f51b2549f0a2
SquidXTV <squidxtv@gmail.com>
2021-08-09 05:18:11 +0200

    Update PharaohRenderer.java

--- src/main/java/com/squidxtv/tlos/entity/PharaohRenderer.java
+++ src/main/java/com/squidxtv/tlos/entity/PharaohRenderer.java
@@ -6,7 +6,7 @@ import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.util.ResourceLocation;
 
 public class PharaohRenderer extends BipedRenderer<PharaohEntity, PharaohModel> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation(TheLegendOfSteve.MODID, "textures/entity/Pharaoh/pharaoh.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(TheLegendOfSteve.MODID, "textures/entity/pharaoh/pharaoh.png");
 
     public PharaohRenderer(EntityRendererManager p_i46168_1_) {
         super(p_i46168_1_, new PharaohModel(0.5F,false), 0.5F);
